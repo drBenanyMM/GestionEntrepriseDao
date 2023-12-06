2d2a6e3ef6b06bf13700ab999ef11c9d9b3396c5
Dr. EL BENANY Mohamed Mahmoud <medmhdbennannu@gmail.com>
2023-12-06 15:34:50 +0100

    Ajout de Employe et EmployeDao CRUD

--- src/mr/liu/dao/EmployeDao.java
+++ src/mr/liu/dao/EmployeDao.java
@@ -74,6 +74,9 @@ public class EmployeDao implements Dao<Employe> {
 		return employe;
 	}
 
+	/*
+	 * SELECT * FROM employe
+	 */
 	@Override
 	public List<Employe> getAll() {
 		// 1 Reqette
@@ -105,23 +108,76 @@ public class EmployeDao implements Dao<Employe> {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-       
-		//6 Rerour de la liste des employes
+
+		// 6 Rerour de la liste des employes
 		return employes;
 	}
 
 	@Override
-	public void save(Employe t) {
+	public void save(Employe employe) {
+		// 1 REquette
+		requette = "INSERT INTO  employe values(" + employe.getNumero() + ",'" + employe.getNom() + "','"
+				+ employe.getGrade() + "','" + employe.getNomDepartment() + "');";
+		System.out.println(requette);
+
+		try {
+			// 2. Recuperer le resulat
+			int resulat = statement.executeUpdate(requette);
+
+			if (resulat != 0)
+				System.out.println("insertion effectué ");
+			else {
+				System.out.println("ERREUR d'insertion  ");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
-	public void update(Employe t, String[] args) {
+	public void update(Employe employe, String[] args) {
+		// 1 REquette
+		requette = "update employe set  numero= " + Integer.parseInt(args[0]) + ", nom= '" + args[1] + "', grade='"
+				+ args[2] + "', nomDepartment='" + args[3] + "' Where numero=" + employe.getNumero() + " ;";
+		System.out.println(requette);
+
+		try {
+			// 2. Recuperer le resulat
+			int resulat = statement.executeUpdate(requette);
+
+			if (resulat != 0)
+				System.out.println("Update effectué ");
+			else {
+				System.out.println("ERREUR d'UPDATE  ");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
-	public void delete(Employe t) {
+	public void delete(Employe employe) {
+		// 1 REquette
+		requette = "DELETE FROM employe Where numero=" + employe.getNumero() + " ;";
+		System.out.println(requette);
+
+		try {
+			// 2. Recuperer le resulat
+			int resulat = statement.executeUpdate(requette);
+
+			if (resulat != 0)
+				System.out.println("Delete effectué ");
+			else {
+				System.out.println("ERREUR de DELETE  ");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -129,10 +185,23 @@ public class EmployeDao implements Dao<Employe> {
 	public static void main(String[] args) {
 		// Test de Select By ID
 		System.out.println("TEST DE LA METODE getById");
-		new EmployeDao().getByID(1212);		
-		//Test Selcet All : getAll()
+		new EmployeDao().getByID(1212);
+		// Test Selcet All : getAll()
 		System.out.println("TEST DE LA METODE getALL()");
 		System.out.println(new EmployeDao().getAll().toString());
+
+		// Test Insert : save()
+		System.out.println("TEST DE LA METODE save()");
+		new EmployeDao().save(new Employe(1214, "Baba", "PRof", "info"));
+
+		// Test Insert : update()
+		System.out.println("TEST DE LA METODE update()");
+		String[] newEmploye = { "1216", "sidi", "PRof", "info" };
+		new EmployeDao().update(new EmployeDao().getByID(1214), newEmploye);
+
+		// Test Insert : DELETE()
+		System.out.println("TEST DE LA METODE DLETE()");
+		new EmployeDao().delete(new EmployeDao().getByID(1216));
 
 	}
 
